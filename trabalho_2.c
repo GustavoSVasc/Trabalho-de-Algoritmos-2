5bfd3e9ace08b77191f42db6e35bb6e041177866
Gustavo Serra Vasconcelos <gustavo.vasconcel.pb@compasso.com.br>
2024-06-19 16:36:43 -0400

    feat: adiciona estrutura inicial do código

---
+++ trabalho_2.c
@@ -0,0 +1,27 @@
+/**********************************
+* Nome dos(as) estudantes: Gustavo Serra Vasconcelos / Pedro Augusto França lima Dutra
+* Trabalho 2
+* Professor: Marco Aurélio 
+*********************************/
+
+#include <stdio.h>
+
+// Função para imprimir o número
+
+
+// Função para somar dois números grandes
+
+
+// Função para subtrair dois números grandes
+
+
+// Função para multiplicar dois números grandes
+
+
+// Função para dividir dois números grandes (divisão inteira)
+
+
+int main(){
+
+    return 0;
+}
